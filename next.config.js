375148659ed01c9c7eb2e6e2177a3a51f60a9f91
onform <ldemetriou33@gmail.com>
2026-01-03 15:25:09 +0400

    Fix: Add engineType binary to Prisma schema and update next.config webpack

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Ensure Prisma client is only used server-side
+      config.externals = [...(config.externals || []), '@prisma/client']
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
