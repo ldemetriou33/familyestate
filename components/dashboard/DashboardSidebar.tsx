97e4209301c79d217f9c82429bc5068aa56fce31
onform <ldemetriou33@gmail.com>
2026-01-05 14:54:15 +0400

    fix: close JSX fragment in DashboardSidebar

--- components/dashboard/DashboardSidebar.tsx
+++ components/dashboard/DashboardSidebar.tsx
@@ -103,6 +103,7 @@ export default function DashboardSidebar({ currentView = 'overview', onNavigate,
       
       {/* Residency Counter */}
       <ResidencyCounter />
-    </div>
+      </div>
+    </>
   )
 }
